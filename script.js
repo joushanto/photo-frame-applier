8d2e7e66b4c7611cb90812007fbf8c72453c0a5e
joushanto <shanto.jouerder@gmail.com>
2023-06-03 11:46:10 +0600

    pan addded

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const downloadBtn = document.getElementById("download-btn");
 const canvas = document.getElementById("canvas");
 const frameImg = document.getElementById("frame");
 const ctx = canvas.getContext("2d");
+const canvasContainer = document.getElementById('canvas-container');
 
 // canvas.width = 500;
 // canvas.height = 500;
@@ -16,6 +17,56 @@ const ctx = canvas.getContext("2d");
 let uploadedImage = null;
 let zoomValue = 1;
 let rotationValue = 0;
+let isPanning = false;
+let panStartX = 0;
+let panStartY = 0;
+let panOffsetX = 0;
+let panOffsetY = 0;
+
+// Event listener for mouse events on the canvas container
+canvasContainer.addEventListener('mousedown', handleMouseDown);
+canvasContainer.addEventListener('mousemove', handleMouseMove);
+canvasContainer.addEventListener('mouseup', handleMouseUp);
+canvasContainer.addEventListener('mouseleave', handleMouseLeave);
+zoomRange.addEventListener('input', handleZoomChange);
+
+
+// Handle zoom range change
+function handleZoomChange(e) {
+  zoomValue = e.target.value / 100;
+  updateCanvas();
+}
+
+
+// Handle mouse down event
+function handleMouseDown(e) {
+  isPanning = true;
+  panStartX = e.clientX;
+  panStartY = e.clientY;
+}
+
+// Handle mouse move event
+function handleMouseMove(e) {
+  if (isPanning) {
+    const deltaX = e.clientX - panStartX;
+    const deltaY = e.clientY - panStartY;
+    panOffsetX += deltaX;
+    panOffsetY += deltaY;
+    panStartX = e.clientX;
+    panStartY = e.clientY;
+    updateCanvas();
+  }
+}
+
+// Handle mouse up event
+function handleMouseUp() {
+  isPanning = false;
+}
+
+// Handle mouse leave event
+function handleMouseLeave() {
+  isPanning = false;
+}
 
 // Default values
 const defaultImageSrc = 'default_image.jpg';
@@ -51,6 +102,7 @@ function handleUpload(e) {
   reader.readAsDataURL(file);
 }
 
+
 // Update the canvas with the current image and adjustments
 function updateCanvas() {
   const imageSize = Math.min(canvas.width, canvas.height);
@@ -61,19 +113,16 @@ function updateCanvas() {
   ctx.save();
 
   ctx.translate(centerX, centerY);
-  ctx.rotate((rotationValue * Math.PI) / 180);
-
+  
   // Calculate the scaled size while maintaining the aspect ratio
-  const scale = Math.max(uploadedImage.width, uploadedImage.height) / imageSize;
-  const scaledWidth = uploadedImage.width / scale;
-  const scaledHeight = uploadedImage.height / scale;
-
+  const scaledWidth = uploadedImage.width * zoomValue;
+  const scaledHeight = uploadedImage.height * zoomValue;
+  
   // Center the image on the canvas
   const offsetX = -scaledWidth / 2;
   const offsetY = -scaledHeight / 2;
 
-  ctx.scale(zoomValue, zoomValue);
-  ctx.drawImage(uploadedImage, offsetX, offsetY, scaledWidth, scaledHeight);
+  ctx.drawImage(uploadedImage, offsetX + panOffsetX, offsetY + panOffsetY, scaledWidth, scaledHeight);
   ctx.restore();
 
   // Apply frame
@@ -82,11 +131,11 @@ function updateCanvas() {
   // Add name overlay
   const name = nameInput.value.trim();
   if (name) {
-    ctx.fillStyle = "#fff"; // Name text color
-    ctx.font = "24px Arial"; // Name font style
-    ctx.textAlign = "center";
-    // ctx.rotate((-15 * Math.PI) / 180);
-    ctx.fillText(name, centerX - 00, centerY - imageSize / 2 + 300); // Adjust position as needed
+    ctx.fillStyle = '#fff'; // Name text color
+    ctx.font = '24px Arial'; // Name font style
+    ctx.textAlign = 'right';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(name, canvas.width - 20, canvas.height - 20);
   }
 }
 
@@ -129,14 +178,50 @@ function cropImage() {
 
   downloadBtn.disabled = false;
 }
-
+``
 // Download the final image
 function downloadImage() {
-  const link = document.createElement("a");
-  link.href = canvas.toDataURL();
-  link.download = "edited_photo.png";
+  const downloadCanvas = document.createElement('canvas');
+  const downloadCtx = downloadCanvas.getContext('2d');
+
+  // Increase canvas size by 4x
+  const scaleFactor = 4;
+  const downloadSize = frameImg.width * scaleFactor;
+  downloadCanvas.width = downloadCanvas.height = downloadSize;
+
+  // Scale down the image
+  downloadCtx.drawImage(
+    canvas,
+    0,
+    0,
+    frameImg.width,
+    frameImg.width,
+    0,
+    0,
+    downloadSize,
+    downloadSize
+  );
+
+  // Apply frame
+  downloadCtx.drawImage(frameImg, 0, 0, downloadSize, downloadSize);
+
+  // Add name overlay
+  const name = nameInput.value.trim();
+  if (name) {
+    downloadCtx.fillStyle = '#fff'; // Name text color
+    downloadCtx.font = '24px Arial'; // Name font style
+    downloadCtx.textAlign = 'right';
+    downloadCtx.textBaseline = 'bottom';
+    downloadCtx.fillText(name, downloadSize - 20, downloadSize - 20);
+  }
+
+  // Download the image
+  const link = document.createElement('a');
+  link.href = downloadCanvas.toDataURL();
+  link.download = 'edited_photo.png';
   link.click();
 }
+
 
 // Event listener for zoom range slider
 zoomRange.addEventListener("input", function (e) {
